910546c582f8954fd23434563d6b1db3a37b0647
cigital <103952468+cigital@users.noreply.github.com>
2022-07-31 23:40:30 +0200

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh 
+
+echo -e "\nDesinstalando requisitos.."
+pip uninstall -r ./requirements.txt -q 
+
+echo "Desinstalando virtualenv..."
+pip uninstall -q virtualenv
+
+echo -e "\Eliminando el directorio..."
+rm -rf entorno
+
+echo -e "\nSe acabo la desinstalación"
